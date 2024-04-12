a6ce35a0a354f93a4fbd44d7da5f997c53580d72
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-04-12 11:13:42 -0600

    update

--- mygame.py
+++ mygame.py
@@ -1,18 +1,24 @@
 import pygame
-
 import sys
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Running Through The City")
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
+        self.img = pygame.image.load("../data/images/clouds/clouds1.png")
 
-screen = pygame.display.set_mode((640, 480))
+    def run(self):
+        while True:
+            self.screen.blit(self.img, (100, 200))
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-clock = pygame.time.Clock()
+            pygame.display.update()
+            self.clock.tick(60)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    pygame.display.update()
-    clock.tick(60)
+Game().run()
