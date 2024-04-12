dccacc3336445d217b865ee0cddc68d13e64eec7
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-04-12 13:24:20 -0600

    character moves up and down

--- mygame.py
+++ mygame.py
@@ -7,16 +7,36 @@ class Game:
         pygame.display.set_caption("Running Through The City")
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
-        self.img = pygame.image.load("../data/images/clouds/clouds1.png")
+        img = pygame.image.load("/Users/donjuan/Downloads/data/images/clouds/file.png")
 
+        self.img = pygame.transform.scale(img, (75, 75))
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+        
     def run(self):
         while True:
-            self.screen.blit(self.img, (100, 200))
+            self.screen.fill((14,219,248))
+            
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img , self.img_pos)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+
+
 
             pygame.display.update()
             self.clock.tick(60)
